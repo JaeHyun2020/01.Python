a46b01ee126bf3af593052e5762728adc9b8d5b7
JaeHyun2020 <polar2000.naver.com>
2024-01-02 22:41:51 +0900

    opencv Start

--- Hello.py
+++ Hello.py
@@ -1,1 +1,9 @@
+import cv2 
+import numpy as np
+import matplotlib.pyplot as plt
+#import requests
+from PIL import Image 
+from io import BytesIO 
+
+print('---------------------------------------------------------------------------------------------------- 001. Start')
 print('Hello Python')
