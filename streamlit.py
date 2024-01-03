07d2fd0c20386899aa088c31d23ba81a335eed10
JaeHyun2020 <polar2000.naver.com>
2024-01-03 22:34:16 +0900

    streamlit 수정

--- streamlit.py
+++ streamlit.py
@@ -21,8 +21,8 @@ st.text('올해 이틀 일함.')
 
 #################################
 dataframe = pd.DataFrame({
-    'first column': [1, 2, 3, 4]
-    'second column': [10, 20, 30, 40]
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],
 })
 
 
