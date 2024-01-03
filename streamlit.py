f23291b2eefe76dc5327c74483cec60ca0ff668b
JaeHyun2020 <polar2000.naver.com>
2024-01-03 21:55:03 +0900

    streamlit 추가

---
+++ streamlit.py
@@ -0,0 +1,18 @@
+import streamlit as st 
+
+st.title('Title')
+
+st.header('Header')
+
+st.subheader('Sub Header')
+
+st.caption('Caption')
+
+sample_code = '''
+this is code
+'''
+
+st.code(sample_code, language='python')
+
+st.text('오늘은 2024년 1월입니다.')
+st.text('올해 이틀 일함.')
